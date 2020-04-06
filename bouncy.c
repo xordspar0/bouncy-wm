a16e5b1ff13793e7c641f899d0192f5daf7283a8
Jordan Christiansen <xordspar0@gmail.com>
2020-04-06 12:58:35 -0500

    Make windows stop at the edge of the screen

--- bouncy.c
+++ bouncy.c
@@ -1,4 +1,5 @@
 #include <X11/Xlib.h>
+#include <stdbool.h>
 #include <unistd.h>
 
 int main()
@@ -9,18 +10,33 @@ int main()
     Window * windows;
     unsigned int nwindows;
     XWindowAttributes attr;
+    int screenWidth, screenHeight;
 
     if(!(display = XOpenDisplay(0x0))) return 1;
 
     root = DefaultRootWindow(display);
 
     for(;;) {
+        XGetWindowAttributes(display, root, &attr);
+        screenWidth = attr.width;
+        screenHeight = attr.height;
+
         XQueryTree(display, root, &root, &parent, &windows, &nwindows);
         for (int i = 0; i < nwindows; i++) {
             XGetWindowAttributes(display, windows[i], &attr);
 
-            int x = attr.x + 1;
-            int y = attr.y + 1;
+            int x, y;
+            if (attr.x + attr.width < screenWidth) {
+                x = attr.x + 1;
+            } else {
+                x = attr.x - 1;
+            }
+            if (attr.y + attr.height < screenHeight) {
+                y = attr.y + 1;
+            } else {
+                y = attr.y - 1;
+            }
+
             XMoveWindow(display, windows[i], x, y);
         }
 
